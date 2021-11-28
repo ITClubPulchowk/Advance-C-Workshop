c59b78cf83c889e58f9b085725f895a82a67995e
Suban <076bct082.suban@pcampus.edu.np>
2021-11-28 07:53:10 +0545

    Initial attempt

---
+++ Day0/suban244/assignment1/ppm.c
@@ -0,0 +1,111 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Pixel {
+  short r, g, b;
+};
+
+void writeCircle(FILE *input, FILE *output, struct Pixel *pixels,
+                 int imageWidth, int imageHeight) {
+  struct Pixel p;
+  int centerX, centerY, radius;
+  int loopStartW, loopStartH, loopEndW, loopEndH;
+
+  fscanf(input, "%d %d %d %hu %hu %hu\n", &centerX, &centerY, &radius, &p.r,
+         &p.g, &p.b);
+  printf("%hu %hu %hu", p.r, p.g, p.b);
+
+  // Capping the for loops
+  loopStartW = (centerX - radius < 0) ? 0 : centerX - radius;
+  loopStartH = (centerY - radius < 0) ? 0 : centerY - radius;
+  loopEndW =
+      (centerX + radius - 1 < imageWidth) ? centerX + radius : imageWidth;
+  loopEndH =
+      (centerY + radius - 1 < imageHeight) ? centerY + radius : imageHeight;
+
+  for (int w = loopStartW; w <= loopEndW; w++) {
+    for (int h = loopStartH; h <= loopEndH; h++) {
+      if (sqrt(pow((w - centerX), 2) + pow((h - centerY), 2)) <= radius) {
+        pixels[imageWidth * h + w] = p;
+      }
+    }
+  }
+}
+
+void writeRect(FILE *input, FILE *output, struct Pixel *pixels, int imageWidth,
+               int imageHeight) {
+
+  struct Pixel p;
+  int centerX, centerY, width, height;
+  int loopStartW, loopStartH, loopEndW, loopEndH;
+  fscanf(input, "%d %d %d %d %hu %hu %hu\n", &centerX, &centerY, &width,
+         &height, &p.r, &p.g, &p.b);
+
+  printf("%hu %hu %hu", p.r, p.g, p.b);
+  // Capping the for loops
+  loopStartW = (centerX - width < 0) ? 0 : centerX - width;
+  loopStartH = (centerY - height < 0) ? 0 : centerY - height;
+  loopEndW = (centerX + width - 1 < imageWidth) ? centerX + width : imageWidth;
+  loopEndH =
+      (centerY + height - 1 < imageHeight) ? centerY + height : imageHeight;
+
+  for (int w = loopStartW; w < loopEndW; w++) {
+    for (int h = loopStartH; h < loopEndH; h++) {
+      pixels[imageWidth * h + w] = p;
+    }
+  }
+}
+
+void writeToFile(FILE *output, struct Pixel *pixels, int imageWidth,
+                 int imageHeight) {
+  for (int i = 0; i < imageHeight * imageWidth; i++) {
+    fprintf(output, "%hu %hu %hu", pixels[i].r, pixels[i].g, pixels[i].b);
+    if (i % imageWidth == imageWidth - 1) {
+      fprintf(output, "\n");
+    } else {
+      fprintf(output, " ");
+    }
+  }
+}
+
+int main() {
+  int imageWidth, imageHeight;
+  char character;
+  struct Pixel *pixels;
+
+  FILE *input = fopen("render.txt", "r");
+  FILE *output = fopen("output.ppm", "w");
+
+  if (input == NULL) {
+    printf("Error");
+    exit(1);
+  }
+
+  fscanf(input, "%d %d\n", &imageWidth, &imageHeight);
+
+  // Write basic info to file
+  fprintf(output, "P3\n");
+  fprintf(output, "%d %d\n", imageWidth, imageHeight);
+  fprintf(output, "255\n");
+  pixels =
+      (struct Pixel *)calloc(imageHeight * imageHeight, (sizeof(struct Pixel)));
+
+  while ((character = fgetc(input)) != EOF) {
+    fgetc(input);
+    switch (character) {
+    case 'c':
+      writeCircle(input, output, pixels, imageWidth, imageHeight);
+      break;
+    case 'r':
+      writeRect(input, output, pixels, imageWidth, imageHeight);
+      break;
+    default:
+      break;
+    }
+  }
+
+  writeToFile(output, pixels, imageWidth, imageHeight);
+  fclose(input);
+  fclose(output);
+}
---
+++ Day0/suban244/assignment2/solvePostfix.c
@@ -0,0 +1,167 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define INPUT_SIZE 300
+#define SYMBOL_SIZE 4
+
+char symbols[SYMBOL_SIZE] = {'+', '-', '/', '*'};
+
+float doMafs(float a, float b, char c) {
+  switch (c) {
+  case '+':
+    return a + b;
+  case '-':
+    return a - b;
+  case '/':
+    return a / b;
+  case '*':
+    return a * b;
+  default:
+    return 0;
+  }
+}
+
+int checkIfSymbol(char a) {
+  for (int i = 0; i < SYMBOL_SIZE; i++) {
+    if (a == symbols[i]) {
+      return 1;
+    }
+  }
+  return 0;
+}
+
+// IDK I think this works
+float recursiveSolver(char **tokens, int tokenCount, int *depth) {
+  int offset = 1;
+  int functionDepth = 0;
+  char symbol = *tokens[tokenCount - offset];
+  offset++;
+  float val1, val2;
+  int isSymbol;
+
+  char *arg1 = tokens[tokenCount - offset];
+  if (!checkIfSymbol(*arg1)) {
+    val1 = atof(arg1);
+    offset++;
+  } else {
+    val1 = recursiveSolver(tokens, tokenCount - offset + 1, &functionDepth);
+  }
+  char *arg2 = tokens[tokenCount - offset - functionDepth];
+  if (!checkIfSymbol(*arg2)) {
+    val2 = atof(arg2);
+  } else {
+    offset--;
+    val2 =
+        recursiveSolver(tokens, tokenCount - offset - functionDepth, &offset);
+  }
+  *depth = offset + functionDepth;
+
+  return doMafs(val2, val1, symbol);
+}
+
+void pushToken(char *token, char **tokens, int *tokenCount) {
+  if (strlen(token) != 0) {
+    tokens[*tokenCount] = token;
+    (*tokenCount) = *tokenCount + 1;
+  }
+}
+
+void subString(char *src, char *dest, int startIndex, int length) {
+  src = src + startIndex;
+  int i;
+  for (i = 0; i < length; i++) {
+    dest[i] = src[i];
+  }
+  dest[i] = '\0';
+}
+
+int createTokens(char expression[], char **tokens) {
+  char *token;
+  int tokenCount = 0;
+
+  // Tokens
+  int startIndex = -1;
+  for (int i = 0; i < strlen(expression); i++) {
+    if (startIndex == -1) {
+      startIndex = i;
+    }
+    if (expression[i] == '\0') {
+      break;
+    }
+
+    if (expression[i] == ' ' && i != strlen(expression) - 1) {
+      char *token = (char *)malloc((i - startIndex + 1) * sizeof(char));
+      subString(expression, token, startIndex, (i - startIndex));
+      pushToken(token, tokens, &tokenCount);
+      startIndex = -1;
+    }
+    // Basically a copy paste of the top and bottom one and i am lazy
+    if (checkIfSymbol(expression[i]) && expression[i - 1] != ' ') {
+      char *token = (char *)malloc((i - startIndex + 1) * sizeof(char));
+      subString(expression, token, startIndex, (i - startIndex));
+      pushToken(token, tokens, &tokenCount);
+      startIndex = -1;
+      token = (char *)malloc((2) * sizeof(char));
+      token[0] = expression[i];
+      token[1] = '\0';
+      pushToken(token, tokens, &tokenCount);
+      continue;
+    }
+
+    if (i == strlen(expression) - 1) {
+      if (expression[i] == ' ')
+        continue;
+      char *token = (char *)malloc((2) * sizeof(char));
+
+      token[0] = expression[i];
+      token[1] = '\0';
+      pushToken(token, tokens, &tokenCount);
+    }
+  }
+  return tokenCount;
+}
+
+void freeTokens(char **tokens, int tokenCount) {
+  for (int i = 0; i < tokenCount; i++) {
+    free(tokens[i]);
+  }
+  free(tokens);
+}
+
+float solve(char *expression) {
+  char **tokens = (char **)malloc(INPUT_SIZE * sizeof(char *));
+  int tokenCount = createTokens(expression, tokens);
+  // printTokens(tokens, tokenCount);
+
+  int depth = 0;
+  // float answer = solveWithStack(tokens, tokenCount);
+  float answer = recursiveSolver(tokens, tokenCount, &depth);
+  freeTokens(tokens, tokenCount);
+  return answer;
+}
+
+int main() {
+  int choice = 1;
+  if (choice == 1) {
+    FILE *input = fopen("expressions.txt", "r");
+    FILE *output = fopen("results.txt", "w");
+    char expression[INPUT_SIZE];
+    float value = 0;
+
+    while (fscanf(input, "%[^\n]", expression) == 1) {
+      fgetc(input);
+
+      value = solve(expression);
+      fprintf(output, "%f\n", value);
+    }
+    fclose(input);
+    fclose(output);
+  } else {
+    char expression[200];
+    scanf("%[^\n]", expression);
+
+    float output = solve(expression);
+    printf("%f\n", output);
+  }
+}
