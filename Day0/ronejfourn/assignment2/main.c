6f8a3c7210ead9da851a3c8728137fda820296a8
r1j4n <76547567+ronejfourn@users.noreply.github.com>
2021-11-28 17:54:21 +0545

    postfixed

--- Day0/ronejfourn/assignment2/main.c
+++ Day0/ronejfourn/assignment2/main.c
@@ -2,6 +2,30 @@
 #include <stdlib.h>
 #include <ctype.h>
 
+#define STK_SIZE 8
+
+typedef struct {
+    float *stack;
+    int cap, ptr;
+} stack;
+
+stack create_stack() {
+    stack ret;
+    ret.stack = malloc(sizeof(*ret.stack) * STK_SIZE);
+    ret.cap = STK_SIZE;
+    ret.ptr = -1;
+    return ret;
+}
+
+void push(stack *stack, unsigned int val){
+    stack->ptr += 1;
+    if (stack->ptr >= stack->cap){
+        stack->cap *= 2;
+        stack->stack = realloc(stack->stack, sizeof(*stack->stack) * stack->cap);
+    }
+    stack->stack[stack->ptr] = val;
+}
+
 int main(int argc, char *argv[]){
     FILE *inp_file = fopen("expressions.txt", "rb");
     if (!inp_file){
@@ -16,49 +40,50 @@ int main(int argc, char *argv[]){
     fread(buffer, 1, size, inp_file);
     fclose(inp_file);
 
+    stack stack = create_stack();
+    int pos = 0;
+
     FILE *out_file = fopen("results.txt", "wb");
-
-    int start = 0, pos = 0;
-    float total = 0;
-
-    while (start < size){
-        int ptr = start;
-        while (buffer[ptr] && buffer[ptr] != '+' && buffer[ptr] != '-' && buffer[ptr] != '*' && buffer[ptr] != '/'){
-            if (buffer[ptr] == '\n'){
-                start = ptr;
-                fprintf(out_file, "%f\n", total);
-                total = 0;
-            }
-            ptr ++;
+    while (pos < size){
+        switch (buffer[pos]) {
+            case '\n':{
+                fprintf(out_file, "%f\n", stack.stack[0]);
+                stack.ptr = -1;
+            } break;
+            case '+':{
+                stack.ptr --;
+                stack.stack[stack.ptr] = stack.stack[stack.ptr] + stack.stack[stack.ptr + 1];
+            } break;
+            case '-':{
+                stack.ptr --;
+                stack.stack[stack.ptr] = stack.stack[stack.ptr] - stack.stack[stack.ptr + 1];
+            } break;
+            case '*':{
+                stack.ptr --;
+                stack.stack[stack.ptr] = stack.stack[stack.ptr] * stack.stack[stack.ptr + 1];
+            } break;
+            case '/':{
+                stack.ptr --;
+                stack.stack[stack.ptr] = stack.stack[stack.ptr] / stack.stack[stack.ptr + 1];
+            } break;
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':{
+                unsigned int a = 0;
+                for (; buffer[pos] >= '0' && buffer[pos] <= '9'; pos ++)
+                    a = a * 10 + buffer[pos] - '0';
+                push(&stack, a);
+            } break;
+            default: break;
         }
-        if (!buffer[ptr]) break;
-        char op = buffer[ptr];
-        while(start < ptr){
-            float a = 0;
-            for (; buffer[start] >= '0' && buffer[start] <= '9'; start ++)
-                a = a * 10 + buffer[start] - '0';
-            if (a){
-                switch(op){
-                    case '+':
-                        total += a;
-                        break;
-                    case '-':
-                        total -= a;
-                        break;
-                    case '*':
-                        total *= a;
-                        break;
-                    case '/':
-                        total /= a;
-                        break;
-                    default: break;
-
-                }
-            }
-            while(isspace(buffer[start])) start ++;
-        }
-        start = ptr + 1;
+        pos ++;
     }
-
     fclose(out_file);
 }
