aa4445d94c00efbce584d0a31cbb355eb77a983c
r1j4n <76547567+ronejfourn@users.noreply.github.com>
2021-11-28 17:16:44 +0545

    pixel perfect

--- Day0/ronejfourn/assignment1/p6.c
+++ Day0/ronejfourn/assignment1/p6.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <ctype.h>
 
 #ifndef max
@@ -9,7 +10,6 @@
 #ifndef min
 #define min(a, b) ((a) < (b) ? (a) : (b))
 #endif
-#define B_THRESHOLD 8
 
 int main(int argc, char *argv[]){
     FILE *inp_file = fopen("render.txt", "rb");
@@ -36,14 +36,15 @@ int main(int argc, char *argv[]){
         img_height = img_height * 10 + buffer[pos] - '0';
     int img_area = img_height * img_width;
 
-    unsigned char *colors = malloc(sizeof(*colors) * img_area * 3);
+    uint8_t *colors = malloc(sizeof(*colors) * img_area * 3);
 
     char type = 1;
     while(pos < size){
         type = buffer[pos];
         if (type == 'c'){
             pos ++;
-            int center_x = 0, center_y = 0, radius = 0, r = 0, g = 0, b = 0;
+            int center_x = 0, center_y = 0, radius = 0;
+            uint8_t r = 0, g = 0, b = 0;
             while(isspace(buffer[pos])) pos ++;
             for (; buffer[pos] >= '0' && buffer[pos] <= '9'; pos ++)
                 center_x = center_x * 10 + buffer[pos] - '0';
@@ -62,35 +63,27 @@ int main(int argc, char *argv[]){
             while(isspace(buffer[pos])) pos ++;
             for (; buffer[pos] >= '0' && buffer[pos] <= '9'; pos ++)
                 b = b * 10 + buffer[pos] - '0';
-            int minm = max(center_y - radius + 1, 0);
-            int maxm = min(center_y + radius, img_height);
+            int minm = max(center_y - radius, 0);
+            int maxm = min(center_y + radius + 1, img_height);
             for(int y = minm; y < maxm; y ++){
-                float chordsqr = radius * radius - (center_y - y) * (center_y - y);
+                int chordsqr = radius * radius - (center_y - y) * (center_y - y);
                 int mn = center_x - radius, mx = center_x;
                 int chord;
-                char ok = 0;
-                for (; mx - mn < B_THRESHOLD;){
+                for (; (mx - mn) > 1;){
                     int mid = (mx + mn) / 2;
-                    float rat = (mid - center_x) * (mid - center_x) / chordsqr;
-                    if (rat > 1) mx = mid;
-                    else if (rat < 1) mn = mid;
-                    else {
-                        ok = 1;
-                        chord = center_x - mid;
-                    }
+                    int rat = (mid - center_x) * (mid - center_x);
+                    if (rat > chordsqr) mn = mid;
+                    else                mx = mid;
                 }
-                for (int i = mn; i < mx && !ok; i ++){
-                    float rat = (i - center_x) * (i - center_x) / chordsqr;
-                    if (rat <= 1) {
-                        chord = center_x - i;
-                        break;
-                    }
-                }
+                if ((mn - center_x) * (mn - center_x) <= chordsqr)
+                    chord = center_x - mn;
+                else
+                    chord = center_x - mx;
                 chord *= 2;
                 int tmp = max(center_x - chord / 2, 0);
                 int pos = y * img_width + tmp;
                 if (!tmp) chord = center_x + chord / 2;
-                chord = min(chord, img_width - tmp);
+                chord = min(chord + 1, img_width - tmp);
                 pos *= 3;
                 for (int i = 0; i < chord * 3; i += 3){
                     colors[pos + i] = r;
@@ -100,7 +93,8 @@ int main(int argc, char *argv[]){
             }
         } else if (type == 'r') {
             pos ++;
-            int center_x = 0, center_y = 0, width = 0, height = 0, r = 0, g = 0, b = 0;
+            int center_x = 0, center_y = 0, width = 0, height = 0;
+            uint8_t r = 0, g = 0, b = 0;
             while(isspace(buffer[pos])) pos ++;
             for (; buffer[pos] >= '0' && buffer[pos] <= '9'; pos ++)
                 center_x = center_x * 10 + buffer[pos] - '0';
@@ -122,10 +116,11 @@ int main(int argc, char *argv[]){
             while(isspace(buffer[pos])) pos ++;
             for (; buffer[pos] >= '0' && buffer[pos] <= '9'; pos ++)
                 b = b * 10 + buffer[pos] - '0';
-            int maxm = min(center_y + height, img_height);
-            for(int y = center_y; y < maxm; y ++){
-                int pos = y * img_width + center_x;
-                int len = min(width, img_width - center_x - 1);
+            int minm = max(center_y - height / 2, 0);
+            int maxm = min(center_y + height / 2 + 1, img_height);
+            for(int y = minm; y < maxm; y ++){
+                int pos = y * img_width + center_x - width / 2;
+                int len = min(width + 1, img_width - center_x - 1);
                 pos *= 3;
                 for (int i = 0; i < len * 3; i += 3){
                     colors[pos + i] = r;
@@ -138,7 +133,7 @@ int main(int argc, char *argv[]){
     }
 
     FILE *out_file = fopen("output.ppm", "wb");
-    fprintf(out_file, "P6\n%d %d\n255\n", img_width, img_height);
+    fprintf(out_file, "P6\n%d %d 255\n", img_width, img_height);
     fwrite(colors, sizeof(*colors), img_area * 3, out_file);
     fclose(out_file);
     return 0;
