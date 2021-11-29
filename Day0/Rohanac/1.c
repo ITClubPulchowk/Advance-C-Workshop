0fa92ae7e6050c4a709ab829bdc7db1e4c3f9420
Rohan <rohanacharya38@gmail.com>
2021-11-29 20:12:45 +0545

    optimised a little

--- Day0/Rohanac/1.c
+++ Day0/Rohanac/1.c
@@ -17,28 +17,19 @@ r center_x center_y width height r g b
 #define maxvalue 255
 int main()
 {
-    struct rect
-    {
-        int center_x;
-        int center_y;
-        int width;
-        int height;
-        int r, g, b;
-    } rect1[4];
-    struct cir
-    {
-        int center_x;
-        int center_y;
-        int radius;
-        int r, g, b;
-    } circle[4];
-    uint32_t **r, **g, **b;
-    char image_data[48];
-    char *file_data;
+
+    uint32_t *pixel_data;
     FILE *fp;
+    int center_x;
+    int center_y;
+    int width;
+    int height;
+    int radius;
+    int r, g, b;
+    char *file_data;
     long file_size;
-    int height, width, count_rect = -1, count_circ = -1;
-    bool shape_change = true, rect = false, circ = false;
+    int img_height, img_width;
+    bool shape_change = true;
     fp = fopen("render.txt", "rb");
     fseek(fp, 0L, SEEK_END);
     file_size = ftell(fp);
@@ -47,31 +38,46 @@ int main()
     fread(file_data, file_size, 1, fp);
     fclose(fp);
     file_data[file_size + 1] = '\0';
-    sscanf(file_data, "%d %d", &width, &height);
-    // rect1=malloc(sizeof(*rect1)*4);
-    // circle=malloc(sizeof(*circle)*4p );
+    sscanf(file_data, "%d %d", &img_width, &img_height);
     while ((*file_data) != '\n')
     {
         file_data++;
     }
+    pixel_data = calloc(img_height * img_width, sizeof(uint32_t));
     while (*(file_data++))
     {
-
         if (shape_change)
         {
             switch (*file_data)
             {
             case 'r':
-                count_rect++;
                 shape_change = false;
-                sscanf(file_data, "r %d %d %d %d %d %d %d\n", &rect1[count_rect].center_x, &rect1[count_rect].center_y, &rect1[count_rect].width, &rect1[count_rect].height, &rect1[count_rect].r, &rect1[count_rect].g, &rect1[count_rect].b);
+                sscanf(file_data, "r %d %d %d %d %d %d %d\n", &center_x, &center_y, &width, &height, &r, &g, &b);
+                for (int i = center_x - width/2; i < center_x + width/2; i++)
+                {
+                    for (int j = center_y - height/2; j < center_y + height/2; j++)
+                    {
+                        if (i < 0 || j < 0)
+                            continue;
+                        pixel_data[i * img_width + j] = (r << 24 | g << 16 | b << 8 | 0xff);
+                    }
+                }
                 break;
 
             case 'c':
                 /* circle */
-                count_circ++;
                 shape_change = false;
-                sscanf(file_data, "c %d %d %d %d %d %d\n", &circle[count_circ].center_x, &circle[count_circ].center_y, &circle[count_circ].radius, &circle[count_circ].r, &circle[count_circ].g, &circle[count_circ].b);
+                sscanf(file_data, "c %d %d %d %d %d %d\n", &center_x, &center_y, &radius, &r, &g, &b);
+                for (int i = 0; i < img_width; i++)
+                {
+                    for (int j = 0; j < img_height; j++)
+                    {
+                        if ((i - center_x) * (i - center_x) + (j - center_y) * (j - center_y) <= radius * radius)
+                        {
+                            pixel_data[j * img_width + i] = (r << 24 | g << 16 | b << 8 | 0xff);
+                        }
+                    }
+                }
                 break;
             default:
                 break;
@@ -83,56 +89,17 @@ int main()
         }
     }
 
-    r = calloc(height, sizeof(uint32_t *));
-    g = calloc(height, sizeof(uint32_t *));
-    b = calloc(height, sizeof(uint32_t *));
-    for (int i = 0; i < width; i++)
-    {
-        r[i] = calloc(width, sizeof(uint32_t));
-        g[i] = calloc(width, sizeof(uint32_t));
-        b[i] = calloc(width, sizeof(uint32_t));
-    }
-    for (int k = 0; k <= count_rect; k++)
-    {
-        for (int i = rect1[k].center_x - rect1[k].width; i < rect1[k].center_x + rect1[k].width; i++)
-        {
-            for (int j = rect1[k].center_y - rect1[k].height; j < rect1[k].center_y + rect1[k].height; j++)
-            {
-                if (i < 0 || j < 0)
-                    continue;
-
-                r[j][i] = rect1[k].r;
-                g[j][i] = rect1[k].g;
-                b[j][i] = rect1[k].b;
-            }
-        }
-    }
-
-    for (int k = 0; k <= count_circ; k++)
-
-    {
-        for (int i = 0; i < width; i++)
-        {
-            for (int j = 0; j < height; j++)
-            {
-                if ((i - circle[k].center_x) * (i - circle[k].center_x) + (j - circle[k].center_y) * (j - circle[k].center_y) <= circle[k].radius * circle[k].radius)
-                {
-                    r[j][i] = circle[k].r;
-                    g[j][i] = circle[k].g;
-                    b[j][i] = circle[k].b;
-                }
-            }
-        }
-    }
     fp = fopen("image.ppm", "wb");
-    fprintf(fp,"P6 %d\n%d\n%d\n", width, height, maxvalue);
-    for (int i = 0; i < height; i++)
+    fprintf(fp, "P6 %d\n%d\n%d\n", img_width, img_height, maxvalue);
+    for (int i = 0; i < img_width; i++)
     {
-        for (int j = 0; j < width; j++)
+        for (int j = 0; j < img_height; j++)
         {
-            fwrite(&g[i][j], sizeof(uint32_t), 1, fp);
-            fwrite(&b[i][j], sizeof(uint32_t), 1, fp);
-            fwrite(&r[i][j], sizeof(uint32_t), 1, fp);
+            unsigned char ar=(pixel_data[j*img_width+i]>>(8*3)) & 0xff;
+            unsigned char ag=(pixel_data[j*img_width+i]>>(8*2)) & 0xff;
+            unsigned char ab=(pixel_data[j*img_width+i]>>(8*1)) & 0xff;
+            fprintf(fp,"%c%c%c",ar,ag,ab);
+
         }
     }
     fclose(fp);
