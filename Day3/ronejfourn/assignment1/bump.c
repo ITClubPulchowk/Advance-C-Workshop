476d1a6171583f306b6b6bcfe8c9f99075048b9e
ronejfourn <rs2102@protonmail.com>
2021-12-08 17:52:17 +0545

    d3a1-windows bump allocator

--- Day3/ronejfourn/assignment1/bump.c
+++ Day3/ronejfourn/assignment1/bump.c
@@ -3,24 +3,32 @@
 
 bump_context bumper;
 
-INLINE uintptr_t align_ptr(void *ptr, size_t alignment){
-    return ((uintptr_t)ptr + alignment - 1) & ~(alignment - 1);
+INLINE void *align_ptr(void *ptr, size_t alignment){
+    uintptr_t hld = ((uintptr_t)ptr + alignment - 1) & ~(alignment - 1);
+    return (void *)hld;
 }
 
 void *bump(size_t size) {
-    if (bumper.cur + size <= bumper.base + bumper.max_size) {
+    // msvc doesnt like pointer arithmetic on void * apparently
+    uintptr_t tmp_cur = (uintptr_t)bumper.cur;
+    uintptr_t tmp_base = (uintptr_t)bumper.base;
+    if (tmp_cur + size <= tmp_base + bumper.max_size) {
         void *tmp = bumper.cur;
-        bumper.cur += size;
+        tmp_cur += size;
+        bumper.cur = (void *) tmp_cur;
         return tmp;
     }
     return NULL;
 }
 
 void *bump_aligned(size_t size, size_t alignment){
-    uintptr_t aligned_cur = align_ptr(bumper.cur, alignment);
-    size_t total_alloc_size = (aligned_cur - (uintptr_t)bumper.cur) + size;
-    if (aligned_cur + total_alloc_size <= (uintptr_t)(bumper.base + bumper.max_size)) {
-        bumper.cur += total_alloc_size;
+    uintptr_t aligned_cur = (uintptr_t)align_ptr(bumper.cur, alignment);
+    uintptr_t tmp_cur = (uintptr_t)bumper.cur;
+    uintptr_t tmp_base = (uintptr_t)bumper.base;
+    size_t total_alloc_size = (aligned_cur - tmp_cur) + size;
+    if (aligned_cur + total_alloc_size <= tmp_base + bumper.max_size) {
+        tmp_cur += total_alloc_size;
+        bumper.cur = (void *)tmp_cur;
         return (void *)aligned_cur;
     }
     return NULL;
@@ -61,4 +69,15 @@ bool vm_free(void *addr, size_t size) {
     return munmap(addr, size) == 0;
 }
 #elif PF_WINDOWS
+#define VC_EXTRALEAN
+#include <Windows.h>
+#include <memoryapi.h>
+void *vm_alloc(size_t size) {
+    void *ret = VirtualAlloc(NULL, size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+    return ret;
+}
+
+bool vm_free(void *addr, size_t size) {
+    return VirtualFree(addr, size, MEM_RELEASE);
+}
 #endif
