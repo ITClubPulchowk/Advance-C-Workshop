c5cecf234155eda344cec1a6ed08211fefeb1542
r1j4n <76547567+ronejfourn@users.noreply.github.com>
2021-12-17 17:26:10 +0545

    d4a1-step2 complete

--- Day4/ronejfourn/assignment1/step1.c
+++ Day4/ronejfourn/assignment1/step1.c
@@ -3,14 +3,14 @@
 #include "bmp_lekhak.h"
 
 uint32_t rgb_to_hex(uint8_t r, uint8_t g, uint8_t b) {
-    return r << 24 | g << 16 | b << 8;
+    return r << 16 | g << 8 | b;
 }
 
 int main() {
     init_bump_context(megabytes(2));
     BMP a = create_bmp(800, 600);
     for (int pos = 0; pos < 600 * 800; pos ++)
-        a.pdata[pos] = rgb_to_hex(0, 0, 255);
-    save_bmp("bruh.bmp", a);
+        a.pdata[pos] = rgb_to_hex(0, 255, 0);
+    save_bmp("image.bmp", a);
     end_bump_context();
 }
--- Day4/ronejfourn/assignment1/step2.c
+++ Day4/ronejfourn/assignment1/step2.c
@@ -1,27 +1,9 @@
+#include <stdint.h>
 #define BMPMALLOC(size) bump(size)
 
+#include <math.h>
 #include "bump.c"
 #include "bmp_lekhak.h"
-
-/* for each pixel (Px, Py) on the screen do */
-/*     x0 := scaled x coordinate of pixel (scaled to lie in the Mandelbrot X scale (-2.00, 0.47)) */
-/*     y0 := scaled y coordinate of pixel (scaled to lie in the Mandelbrot Y scale (-1.12, 1.12)) */
-/*     x := 0.0 */
-/*     y := 0.0 */
-/*     iteration := 0 */
-/*     max_iteration := 1000 */
-/*     x2 := 0 */
-/*     y2 := 0 */
-/*  */
-/*     while (x2 + y2 ≤ 4 and iteration < max_iteration) do */
-/*         y := 2 × x × y + y0 */
-/*         x := x2 - y2 + x0 */
-/*         x2 := x × x */
-/*         y2 := y × y */
-/*         iteration := iteration + 1 */
-/*  */
-/*     color := palette[iteration] */
-/*     plot(Px, Py, color) */
 
 #define IM_WIDTH 800
 #define IM_HEIGHT 600
@@ -33,7 +15,7 @@
 #define MAX_ITER 1000
 
 uint32_t rgb_to_hex(uint8_t r, uint8_t g, uint8_t b) {
-    return r << 24 | g << 16 | b << 8;
+    return r << 16 | g << 8 | b;
 }
 
 float scale(float pmin, float pmax, float nmin, float nmax, float value) {
@@ -41,17 +23,43 @@ float scale(float pmin, float pmax, float nmin, float nmax, float value) {
     return ret;
 }
 
+float lerp (float v0, float v1, float t) {
+    return (1 - t) * v0 + t * v1;
+}
+
+uint32_t get_color(float t) {
+    uint8_t r = 0, g = 0, b = 0;
+
+    if (t >= 0.0 && t < 0.16) {
+        t = (t - 0) / (0.16 - 0);
+        r = lerp(0  , 32 , t);
+        g = lerp(7  , 107, t);
+        b = lerp(100, 203, t);
+    } else if (t >= 0.16 && t < 0.42) {
+        t = (t - 0.16) / (0.42 - 0.16);
+        r = lerp(32 , 237, t);
+        g = lerp(107, 255, t);
+        b = lerp(203, 255, t);
+    } else if (t >= 0.42 && t < 0.6425) {
+        t = (t - 0.42) / (0.6425 - 0.42);
+        r = lerp(237, 255, t);
+        g = lerp(255, 170, t);
+        b = lerp(255, 0  , t);
+    } else if (t >= 0.6425 && t < 0.8575) {
+        t = (t - 0.6425) / (0.8575 - 0.6425);
+        r = lerp(255, 0  , t);
+        g = lerp(170, 2  , t);
+        b = lerp(0  , 0  , t);
+    } else {
+        g = 2;
+    }
+
+    return rgb_to_hex(r, g, b);
+}
+
 int main() {
     init_bump_context(megabytes(512));
     BMP image = create_bmp(IM_WIDTH, IM_HEIGHT);
-
-    uint32_t palette[] = {
-        rgb_to_hex(  0,   2,   0),
-        rgb_to_hex(255, 170,   0),
-        rgb_to_hex(237, 255, 255),
-        rgb_to_hex( 32, 107, 203),
-        rgb_to_hex(  0,   7, 100)
-    };
 
     for (int Py = 0; Py < IM_HEIGHT / 2; Py ++) {
         float y0 = scale(0, IM_HEIGHT, MN_Y_SCALE_MIN, MN_Y_SCALE_MAX, Py);
@@ -66,8 +74,9 @@ int main() {
                 y2 = y * y;
                 iteration += 1;
             }
-            image.pdata[Py * IM_WIDTH + Px] = palette[iteration % 5];
-            image.pdata[(IM_HEIGHT - Py - 1) * IM_WIDTH + Px] = palette[iteration % 5];
+            uint32_t color = get_color((float)iteration / MAX_ITER);
+            image.pdata[Py * IM_WIDTH + Px] = color;
+            image.pdata[(IM_HEIGHT - Py - 1) * IM_WIDTH + Px] = color;
         }
     }
 
