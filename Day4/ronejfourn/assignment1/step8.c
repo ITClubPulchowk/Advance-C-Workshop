bd818d6f7c8250c22b0267a354c6ba57f6d0627a
r1j4n <76547567+ronejfourn@users.noreply.github.com>
2021-12-22 20:50:26 +0545

    d4a1-row 0 being recalculated fixed

--- Day4/ronejfourn/assignment1/step8.c
+++ Day4/ronejfourn/assignment1/step8.c
@@ -6,8 +6,9 @@ volatile uint32_t in;
 
 int draw(void *pxl) {
     uint32_t *can = (uint32_t *) pxl;
-    for (; in < IM_HEIGHT / 2; interlocked_inc(&in)) {
-        uint32_t Py = in;
+    interlocked_inc(&in);
+    for (; in <= IM_HEIGHT / 2; interlocked_inc(&in)) {
+        uint32_t Py = in - 1;
         float y0 = map_range(0, IM_HEIGHT, MN_Y_SCALE_MIN, MN_Y_SCALE_MAX, Py);
         for(int Px = 0; Px < IM_WIDTH; Px ++) {
             float x0 = map_range(0, IM_WIDTH, MN_X_SCALE_MIN, MN_X_SCALE_MAX, Px);
@@ -36,6 +37,7 @@ int main() {
     fprintf(outf, "Threads,Performance\n");
     for (uint32_t thread_count = 1; thread_count <= get_core_count() * 2; thread_count ++){
         progress = 0;
+        in = 0;
         printf("Thread Count: %10d\n", thread_count);
         start_timing(&tmr);
         BMP image = create_bmp(IM_WIDTH, IM_HEIGHT);
