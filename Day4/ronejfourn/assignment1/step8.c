34620505f02b03cd31ec717c22cb9e0884e70421
ronejfourn <rs2102@protonmail.com>
2021-12-22 19:52:25 +0545

    d4a1-row-wise calculations

--- Day4/ronejfourn/assignment1/step8.c
+++ Day4/ronejfourn/assignment1/step8.c
@@ -2,15 +2,12 @@
 #include "threading.h"
 
 volatile uint32_t progress;
-typedef struct {
-    uint32_t *chunk;
-    uint32_t start;
-    uint32_t height;
-} canvas;
+volatile uint32_t in;
 
 int draw(void *pxl) {
-    canvas *can = (canvas *) pxl;
-    for (int Py = can->start; Py < can->start + can->height && Py <= IM_HEIGHT / 2; Py ++) {
+    uint32_t *can = (uint32_t *) pxl;
+    for (; in < IM_HEIGHT / 2; interlocked_inc(&in)) {
+        uint32_t Py = in;
         float y0 = map_range(0, IM_HEIGHT, MN_Y_SCALE_MIN, MN_Y_SCALE_MAX, Py);
         for(int Px = 0; Px < IM_WIDTH; Px ++) {
             float x0 = map_range(0, IM_WIDTH, MN_X_SCALE_MIN, MN_X_SCALE_MAX, Px);
@@ -24,8 +21,8 @@ int draw(void *pxl) {
                 iteration += 1;
             }
             uint32_t color = get_color(iteration);
-            can->chunk[Py * IM_WIDTH + Px] = color;
-            can->chunk[(IM_HEIGHT - Py - 1) * IM_WIDTH + Px] = color;
+            can[Py * IM_WIDTH + Px] = color;
+            can[(IM_HEIGHT - Py - 1) * IM_WIDTH + Px] = color;
             interlocked_inc(&progress);
         }
     }
@@ -44,21 +41,9 @@ int main() {
         BMP image = create_bmp(IM_WIDTH, IM_HEIGHT);
 
         thrd_t *rendering_threads = bump(thread_count * sizeof(*rendering_threads));
-        uint32_t height_for_threads = IM_HEIGHT / 2 / thread_count;
-        canvas *a = bump(thread_count * sizeof(*a));
 
         for (int i = 0; i < thread_count; i ++) {
-            a[i].chunk  = image.pdata;
-            a[i].start  = i * height_for_threads;
-            a[i].height = height_for_threads;
-        }
-
-        if (a[thread_count - 1].start + a[thread_count - 1].height != IM_HEIGHT / 2) {
-            a[thread_count - 1].height = IM_HEIGHT / 2 - a[thread_count - 1].start;
-        }
-
-        for (int i = 0; i < thread_count; i ++) {
-            thrd_create(&rendering_threads[i], draw, &a[i]);
+            thrd_create(&rendering_threads[i], draw, image.pdata);
         }
 
         while (progress < IM_WIDTH * IM_HEIGHT / 2) {
