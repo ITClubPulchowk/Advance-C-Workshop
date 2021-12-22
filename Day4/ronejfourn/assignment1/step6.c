386452590300ac8a6c6bf4317959cf0c50c8ad4b
r1j4n <76547567+ronejfourn@users.noreply.github.com>
2021-12-22 18:16:07 +0545

    d4a1-moved common code to header file

--- Day4/ronejfourn/assignment1/step6.c
+++ Day4/ronejfourn/assignment1/step6.c
@@ -1,75 +1,6 @@
-#define BMPMALLOC(size) bump(size)
-
-#include "bump.c"
-#include "bmp_lekhak.h"
+#include "mdlbrt_cmn.h"
 #include "threading.h"
 #include <stdlib.h>
-
-#define IM_WIDTH 800
-#define IM_HEIGHT 600
-
-#define MN_X_SCALE_MIN -2.00
-#define MN_X_SCALE_MAX 0.47
-#define MN_Y_SCALE_MIN -1.12
-#define MN_Y_SCALE_MAX 1.12
-#define MAX_ITER 1000
-
-#if PF_LINUX
-#include <unistd.h>
-#define get_core_count() sysconf(_SC_NPROCESSORS_CONF)
-#elif PF_WINDOWS
-#define VC_EXTRALEAN
-#include <Windows.h>
-#include <sysinfoapi.h>
-long get_core_count() {
-    SYSTEM_INFO tmp;
-    GetSystemInfo(&tmp);
-    return tmp.dwNumberOfProcessors;
-}
-#endif
-
-uint32_t rgb_to_hex(uint8_t r, uint8_t g, uint8_t b) {
-    return r << 16 | g << 8 | b;
-}
-
-float map_range(float pmin, float pmax, float nmin, float nmax, float value) {
-    float ret = (value - pmin) * (nmax - nmin) / (pmax - pmin) + nmin;
-    return ret;
-}
-
-float lerp (float v0, float v1, float t) {
-    return (1 - t) * v0 + t * v1;
-}
-
-uint32_t get_color(float t) {
-    uint8_t r = 0, g = 0, b = 0;
-
-    if (t >= 0.0 && t < 0.16) {
-        t = (t - 0) / (0.16 - 0);
-        r = lerp(0  , 32 , t);
-        g = lerp(7  , 107, t);
-        b = lerp(100, 203, t);
-    } else if (t >= 0.16 && t < 0.42) {
-        t = (t - 0.16) / (0.42 - 0.16);
-        r = lerp(32 , 237, t);
-        g = lerp(107, 255, t);
-        b = lerp(203, 255, t);
-    } else if (t >= 0.42 && t < 0.6425) {
-        t = (t - 0.42) / (0.6425 - 0.42);
-        r = lerp(237, 255, t);
-        g = lerp(255, 170, t);
-        b = lerp(255, 0  , t);
-    } else if (t >= 0.6425 && t < 0.8575) {
-        t = (t - 0.6425) / (0.8575 - 0.6425);
-        r = lerp(255, 0  , t);
-        g = lerp(170, 2  , t);
-        b = lerp(0  , 0  , t);
-    } else {
-        g = 2;
-    }
-
-    return rgb_to_hex(r, g, b);
-}
 
 volatile uint32_t progress;
 typedef struct {
@@ -93,7 +24,7 @@ int draw(void *pxl) {
                 y2 = y * y;
                 iteration += 1;
             }
-            uint32_t color = get_color((float)iteration / MAX_ITER);
+            uint32_t color = get_color(iteration);
             can->chunk[Py * IM_WIDTH + Px] = color;
             can->chunk[(IM_HEIGHT - Py - 1) * IM_WIDTH + Px] = color;
             interlocked_inc(&progress);
